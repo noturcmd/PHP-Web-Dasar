1aa23c10ae1012717cfcc18dc021ad53c70308ea
noturcmd <ridwan170604bakhri@gmail.com>
2023-07-27 22:05:29 +0700

    test3

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -8,13 +8,14 @@
 </head>
 
 <body>
-    <table>
+    <table border="1" cellpadding="10" cellspacing="0">
         <?php 
             for($i = 1; $i <= 3; $i++){ ?>
         <tr>
-            <?php ?>
+            <?php for($j = 1; $j <= 5; $j++){ ?>
+            <td><?php echo "$i, $j" ?></td>
+            <?php } ?>
         </tr>
-
         <?php } ?>
     </table>
 </body>
