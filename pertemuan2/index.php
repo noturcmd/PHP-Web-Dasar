9a427f566e3ce39b65a27ed0d825ba2f066b5ae3
noturcmd <ridwan170604bakhri@gmail.com>
2023-07-28 02:40:37 +0700

    test4

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -10,13 +10,13 @@
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
         <?php 
-            for($i = 1; $i <= 3; $i++){ ?>
+            for($i = 1; $i <= 3; $i++): ?>
         <tr>
             <?php for($j = 1; $j <= 5; $j++){ ?>
             <td><?php echo "$i, $j" ?></td>
             <?php } ?>
         </tr>
-        <?php } ?>
+        <?php endfor; ?>
     </table>
 </body>
 
